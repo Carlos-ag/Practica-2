d7206ba2d027732c55c0c197d9381119d4258c93
Carlos-ag <carlosmartindeargila@outlook.es>
2024-02-19 19:44:53 +0100

    cambios

--- Apartado 1.2/client/main.py
+++ Apartado 1.2/client/main.py
@@ -8,46 +8,36 @@ print_lock = threading.Lock()
 
 already_printed_server_not_working = False
 
-contador = 0
-
-def send_message(server_address, port, message, contador):
-    
+def send_message(server_address, port, message):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             with print_lock:
-                print(f"{contador} : ", end = "")
                 sock.connect((server_address, port))
                 sock.sendall(message.encode())
                 response = sock.recv(1024).decode()
-                print(f"Server response: {response}", end = "\n")
+                print(f"Server response: {response}")
     except:
         if (not already_printed_server_not_working):
-            print("Server not working", end = "\n")
+            print("Server not working")
             already_printed_server_not_working = True
         
 
 def main():
-    global contador
     server_ip, server_port = ask_ip_and_port()
-    # server_ip = "localhost"
-    # server_port = 6968
     messages = []
     message = ""
     while message != "exit":
         message = input("Enter message (type 'exit' to finish): ")
         messages.append(message)
     
-    # threads = []
-    # no estabamos enviando el "exit" al servidor, por eso no cerraba ->
-    # messages.append(message)
-    for message in messages: 
-        contador += 1
-        t = threading.Thread(target=send_message, args=(server_ip, server_port, message, contador))
-        # threads.append(thread)
-        t.start()
+    threads = []
+    for message in messages[:-1]: 
+        thread = threading.Thread(target=send_message, args=(server_ip, server_port, message))
+        threads.append(thread)
+        thread.start()
     
-    # for thread in threads:
-    #     thread.join()
+    for thread in threads:
+        thread.join()
 
 
     

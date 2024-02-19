e0d1d25505c45f14e9ce58d5c6a66d4d6510ad7f
GitDeMike <97988392+GitDeMike@users.noreply.github.com>
2024-02-19 20:18:40 +0100

    1.2 terminado

--- Apartado 1.2/client/main.py
+++ Apartado 1.2/client/main.py
@@ -8,21 +8,31 @@ print_lock = threading.Lock()
 
 already_printed_server_not_working = False
 
+contador = 0
+
 def send_message(server_address, port, message):
+    global contador
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            with print_lock:
-                sock.connect((server_address, port))
-                sock.sendall(message.encode())
-                response = sock.recv(1024).decode()
-                print(f"Server response: {response}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        with print_lock:
+            contador += 1
+            sock.connect((server_address, port)) 
+            print(f" {contador}: Sending message: {message}")
+            sock.sendall(message.encode())  
+            response = sock.recv(1024).decode()  
+            print(f" {contador}: Server response: {response}")
+            sock.close()  
+            print(f" {contador}: Connection closed\n")        
+            
     except:
         if (not already_printed_server_not_working):
             print("Server not working")
             already_printed_server_not_working = True
+
         
 
 def main():
+    global contador
     server_ip, server_port = ask_ip_and_port()
     messages = []
     message = ""
